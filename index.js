5cb793a836fe0c589401e48d4a0444105aa05a72
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-03-25 23:37:11 -0500

    organized functions

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 import books from "/books.js";
-let currentBook = books[Math.floor(Math.random() * 6)];
 let currentIndex = 0;
+let totalWords = 0;
 let testBody = document.getElementById("testing-body");
-testBody.innerHTML = [...currentBook]
-  .toSpliced(0, 1, `<span class='current'>${currentBook[0]}</span>`)
-  .join("");
-
-document.addEventListener("keydown", (e) => {
-  let pressedKey = e.key;
-  if (e.key === " ") {
-    pressedKey = "space";
-  }
-  document.getElementById(pressedKey.toUpperCase()).classList.add("pressed");
-  e.key !== "Shift" && nextLetter(e.key);
-  setTimeout(
-    () =>
-      document
-        .getElementById(pressedKey.toUpperCase())
-        .classList.remove("pressed"),
-    250
-  );
-});
+startTest();
 
 function nextLetter(key) {
   if (testBody.innerText[currentIndex] === " ") {
     testBody.innerText = testBody.innerText.substring(currentIndex + 1);
     currentIndex = 0;
+    totalWords += 1;
+    document.getElementById("score").innerText = totalWords;
     testBody.innerHTML = [...testBody.innerText]
       .toSpliced(0, 1, `<span class='current'>${testBody.innerText[0]}</span>`)
       .join("");
@@ -57,4 +41,43 @@ function nextLetter(key) {
     );
   }
   currentIndex += 1;
+}
+function startTest() {
+  let currentBook = books[Math.floor(Math.random() * 6)];
+  testBody.innerHTML = [...currentBook]
+    .toSpliced(0, 1, `<span class='current'>${currentBook[0]}</span>`)
+    .join("");
+
+  document.addEventListener("keydown", (e) => {
+    let pressedKey = keyProcessor(e);
+    document.getElementById(pressedKey).classList.add("pressed");
+    e.key !== "Shift" && nextLetter(e.key);
+  });
+
+  document.addEventListener("keyup", (e) => {
+    let pressedKey = keyProcessor(e);
+    setTimeout(
+      () => document.getElementById(pressedKey).classList.remove("pressed"),
+      100
+    );
+  });
+}
+
+function endTest() {
+  document.removeEventListener;
+}
+
+function keyProcessor(event) {
+  switch (event.key) {
+    case " ":
+      return "SPACE";
+    case '"':
+      return "quote";
+    case "Shift":
+      return event.code;
+    case "Tab":
+      return;
+    default:
+      return event.key.toUpperCase();
+  }
 }
